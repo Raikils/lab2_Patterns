d7c38e272e3d144c35b954ae1be7088e6987be61
Raikils <70800499+Raikils@users.noreply.github.com>
2021-04-11 12:09:14 +0300

    Improved Rabina Karpa

--- SubstringSearchAlgorithms/Decorator.cpp
+++ SubstringSearchAlgorithms/Decorator.cpp
@@ -4,3 +4,10 @@ int Decorator::Search(const std::string& Line_1, const std::string& Line_2)
 {
 	return w->Search(Line_1, Line_2);
 }
+
+int Improved_Rabina_Karpa::Search(const std::string& Line_1, const std::string& Line_2)
+{
+	r->q = 18014398241046527;
+	r->d = std::rand() + 1;
+	return r->Search(Line_1, Line_2);
+}
--- SubstringSearchAlgorithms/Decorator.h
+++ SubstringSearchAlgorithms/Decorator.h
@@ -7,8 +7,17 @@ class Decorator: public Substring_Search_Algorithms
 private:
 	Substring_Search_Algorithms* w;
 public:
-	Decorator(Substring_Search_Algorithms* w): w{w} {}
-	Decorator(): w{nullptr} {}
+	Decorator(Substring_Search_Algorithms* w): w(w) {}
+	Decorator(): w(nullptr) {}
 	int Search(const std::string& Line_1, const std::string& Line_2);
 };
 
+class Improved_Rabina_Karpa : public Rabina_Karpa
+{
+private:
+	Rabina_Karpa* r;
+public:
+	Improved_Rabina_Karpa(Rabina_Karpa* r) : r(r) {}
+	Improved_Rabina_Karpa() : r(new Rabina_Karpa) {}
+	int Search(const std::string& Line_1, const std::string& Line_2);
+};
