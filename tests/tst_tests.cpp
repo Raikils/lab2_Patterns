bf3ce4d4bb169147eaa63647fb99ad9907af842b
Raikils <70800499+Raikils@users.noreply.github.com>
2021-04-20 09:52:59 +0300

    More tests
    Add more tests

--- tests/tst_tests.cpp
+++ tests/tst_tests.cpp
@@ -10,6 +10,8 @@
 #include "Decorator.cpp"
 #include "Timer.h"
 #include "Timer.cpp"
+#include <QDebug>
+#include <string>
 
 // add necessary includes here
 
@@ -26,6 +28,7 @@ private slots:
     void CheckRabina_Karpa();
     void CheckHorspool();
     void CheckKMP();
+    void CheckBoyer_Moor();
     void cleanupTestCase();
     void test_case1();
 
@@ -101,6 +104,25 @@ void Tests::CheckHorspool()
 void Tests::CheckKMP()
 {
     Substring_Search_Algorithms_* a = new Substring_Search_Algorithms_(new KMP);
+    QVERIFY(a->Search("abjfjdihfdn.dds123da","fdn.dds1") == 8);
+    QVERIFY(a->Search("ldmemmvlds,l;sllel,e22","e22") == 19);
+    QVERIFY(a->Search("","fdn.dds1") == -1);
+    QVERIFY(a->Search("","") == -1);
+    QVERIFY(a->Search("fsawdsadsa","") == -1);
+    QVERIFY(a->Search("dwdewfewfewf","dwdewf") == 0);
+    QVERIFY(a->Search("vsdffefefewfewfefdscwefewf","efefewfewfefds1") == -1);
+    QVERIFY(a->Search("rvrfrfwwfdsfvcxdwewfew","fwwfdsfvcxdwew2") == -1);
+    QVERIFY(a->Search("vjmfkmkrmfkewmflkmejkwnfgnrjkgnjknrjgnfrwknfjknwjnflewngljfnlgnjlrenfjgnrejnglk;ergregq","mejkwnfgnrjkgnjknrjgnfrw") == 17);
+    QVERIFY(a->Search("ytjytjytjteyjejruyfjgifmdkgimikfdimgp","ytjytjteyjejruyfjgifmdkgimikfdi") == 3);
+    QVERIFY(a->Search("1234567888654351332","78886543") == 6);
+    QVERIFY(a->Search("43jirm4j3fij3ji4f3f4i34-f","m4j3fij3ji4f3f") == 5);
+    QVERIFY(a->Search("1111","11111") == -1);
+    QVERIFY(a->Search("gregergerfes","greger") == 0);
+}
+
+void Tests::CheckBoyer_Moor()
+{
+    Substring_Search_Algorithms_* a = new Substring_Search_Algorithms_(new Boyer_Moor);
     QVERIFY(a->Search("abjfjdihfdn.dds123da","fdn.dds1") == 8);
     QVERIFY(a->Search("ldmemmvlds,l;sllel,e22","e22") == 19);
     QVERIFY(a->Search("","fdn.dds1") == -1);
