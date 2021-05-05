aed3044ee2dc9ea7b43c9749da1459b0d7162010
Raikils <70800499+Raikils@users.noreply.github.com>
2021-05-05 08:44:36 +0300

    Add documentation

--- SubstringSearchAlgorithms/Decorator.h
+++ SubstringSearchAlgorithms/Decorator.h
@@ -1,16 +1,37 @@
 #pragma once
 #include "Strategy.h"
-
+/*!
+ * \brief The Decorator pattern
+ * This class implements Decorator pattern
+ */
 class Decorator: public Substring_Search_Algorithms
 {
 private:
 	Substring_Search_Algorithms* w;
 public:
+    /*!
+     * \brief Decorator
+     * Сonstructor with one parameter w
+     */
 	Decorator(Substring_Search_Algorithms* w): w(w) {}
+    /*!
+     * \brief Decorator
+     * Standart constructor
+     */
 	Decorator(): w(nullptr) {}
+    /*!Method accept for pattern visitor*/
 	std::string accept(class Visitor& v, const std::string& Line_1, const std::string& Line_2);
-        std::vector<point> GetBreak_();
+    /*!
+     * \brief GetBreak_
+     * This method returns array of break points
+     */
+    std::vector<point> GetBreak_();
+    /*!
+     * \brief SetBreak_
+     * This method adds break point
+     */
 	void SetBreak_(const int& i, const int& j, const bool& q);
+    /*!Method for finding a substring*/
 	int Search(const std::string& Line_1, const std::string& Line_2);
 };
 
--- SubstringSearchAlgorithms/Timer.h
+++ SubstringSearchAlgorithms/Timer.h
@@ -2,13 +2,26 @@
 #include "Decorator.h"
 #include <ctime>
 
+/*!
+ * \brief The Timer class
+ * This class measures time
+ */
 
 class  Timer : public Decorator
 {
 private:
         double _time;
 public:
+        /*!
+         * \brief Timer
+         * Сonstructor with one parameter w
+         */
         Timer(Substring_Search_Algorithms* w);
+        /*!Method for finding a substring*/
 	int Search(const std::string& Line_1, const std::string& Line_2);
+    /*!
+         * \brief time
+         * This method returns running time of the last algorithm
+         */
         double time();
 };
